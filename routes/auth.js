2cfc8cb47baf0771e60294d2696963e66e73bfe5
moosah01 <moosa.hashim@nixorcollege.edu.pk>
2023-03-30 20:43:47 +0500

    first bomb blast => user created

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const authService = require("../services/authService");
 const asyncMiddleware = require("../middlewares/asyncMiddleware");
 
 router.get("/test", asyncMiddleware(authService.test));
+router.post("/createUser", asyncMiddleware(authService.createUser));
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,3 +1,33 @@
+const User = require("../models/User.js");
+
 exports.test = (req, res, next) => {
   res.send("Test");
+};
+
+exports.createUser = async (req, res, next) => {
+  
+  const user = new User({
+    email: "moosah01@gmail.com",
+    fullName: "Moosa Hashim",
+    ERP: "2018A7PS0001H",
+    userType: "Student",
+    notifications: [],
+    courses: [],
+    password: "123456",
+    profilePic: "https://placeholder.png",
+    phoneNumber: "0000000000",
+    CGPA: 0.0,
+    Program: "6425a66e47dcb940dfee5b59",
+
+  }); 
+  const result = await user.save();
+  if (result) {
+    res.status(200).send({
+      result,
+    });
+  } else {
+    res.status(400).send({
+      message: "Error creating user",
+    })
+  }
 };
