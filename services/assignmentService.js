31d51ed76dfad373aae9c92d10c0bf89a45284fe
saadkarim247 <saadkarim247@gmail.com>
2023-04-30 15:22:05 +0500

    create Assignent with file upload

--- services/assignmentService.js
+++ services/assignmentService.js
@@ -1,8 +1,34 @@
-const {Assignment, validateAssignment} = require("../models/Assignment");
-
+const { Assignment, validateAssignment } = require("../models/Assignment");
 
 exports.createAssignment = async (req, res, next) => {
-    
-res.json()
+//   console.log(req.body);
+//   console.log(req.file);
+  const assignmentSchema = {
+    uploadDate: Date.now(),
+    dueDate: req.body?.dueDate,
+    title: req.body?.title,
+    classId: req.body?.classId,
+    resubmissionsAllowed: req.body?.resubmissionsAllowed,
+    status: req.body?.status,
+    resubmissionDeadline: req.body?.resubmissionDeadline,
+    description: req.body?.description,
 
-}
+    filename: req.file?.filename,
+    filePath: req.file?.path,
+    fileSize: req.file?.size,
+    fileType: req.file?.mimetype,
+
+    submissions: [],
+    marks: req.body?.marks,
+  };
+
+  const { error } = validateAssignment(assignmentSchema);
+  if (error)
+    return res.status(400).send({ message: `${error.details[0].message}` });
+
+  let assignment = new Assignment(assignmentSchema);
+
+  const result = await assignment.save();
+
+  res.json(result);
+};
