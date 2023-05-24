5e1f70c14e8ca8d9367afcec1c6b64769dd8b975
saadkarim247 <saadkarim247@gmail.com>
2023-05-24 15:52:14 +0500

    create user admin fixed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       //"mongodb+srv://vercel-admin-user:admin123@massh.hk78ufw.mongodb.net/test2"
-      "mongodb+srv://saadKarim:admin123@massh.hk78ufw.mongodb.net/test1"
+      "mongodb+srv://saadKarim:admin123@massh.hk78ufw.mongodb.net/test"
     );
     // conn.once("open", () => {
     //   var GFS = Grid(conn.db, mongo);
